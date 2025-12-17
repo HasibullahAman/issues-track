392fe071da7e8a8907d90547ff7e2ca6fcc84759
HasibullahAman <hasib.aman7@gmail.com>
2025-12-17 05:42:49 +0430

    done NavBar Style

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,40 @@
+"use client"
 import Link from 'next/link'
 import { AiFillBug } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const NabBar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
+
+    const links = [
+        { name: 'Dashboard', href: "/" },
+        { name: 'Issues', href: "/issues" },
+    ]
     return (
-        <nav className='flex space-x-6 border-b mb-5 px-4 h-14 items-center'>
-            <Link href="/"><AiFillBug /></Link>
-            <ul className='flex space-x-6'>
-                <li><Link className='text-zinc-500 hover:text-zinc-800' href="/">Dashbord</Link></li>
-                <li><Link href="/">Issues</Link></li>
+        <nav className="flex space-x-6 border-b mb-5 px-4 h-14 items-center">
+            <Link href="/" className="text-xl"><AiFillBug /></Link>
+            <ul className="flex space-x-6">
+                {links.map(link => (
+                    <li key={link.href}>
+                        <Link
+                            href={link.href}
+                            className={classnames(
+                                {
+                                    'text-zinc-900': link.href === currentPath,
+                                    'text-zinc-500': link.href !== currentPath
+                                },
+                                'hover:text-zinc-800 transition-colors'
+                            )}
+                        >
+                            {link.name}
+                        </Link>
+                    </li>
+                ))}
             </ul>
-        </nav>
-    )
+        </nav >
+    );
 }
 
 export default NabBar
-      
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuesPpage = () => {
+    return (
+        <div>
+            Welcome to issues page
+        </div>
+    )
+}
+
+export default IssuesPpage
